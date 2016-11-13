469474eef3fc5aa948a432e51da312179cebb02a
Benjamin Lamb <bennyfactor@iaatb.net>
2016-11-13 00:40:37 -0500

    extract old inline css to seperate file

--- home.php
+++ home.php
@@ -7,7 +7,7 @@
 		<title>iaatb.net</title>
 		<?php
 			$cssroot = function_exists('get_stylesheet_directory_uri') ? get_stylesheet_directory_uri() . '/css' : '/css' ;
-      $fileroot = function_exists('get_stylesheet_directory_uri') ? get_stylesheet_directory_uri() . '' : '/' ;
+      $fileroot = function_exists('get_stylesheet_directory_uri') ? get_stylesheet_directory_uri() . '' : '' ;
 		?>
 		<link rel="stylesheet" href="<?php echo $cssroot ?>/style.css" type="text/css" media="screen" />
 		<script type="text/javascript" src="<?php echo $fileroot ?>/js/public.js"></script>
@@ -17,15 +17,15 @@
 	</head>
 <div class="footer" id="footer"><div id="footer-spacer">&nbsp;</div><div id="footer-image">&nbsp;</div></div>
 <div class="content">
-		<img src="<?php echo $fileroot ?>/main/iaatblarge-t.png" />
+		<img src="<?php echo $fileroot ?>/images/iaatblarge-t.png" />
 		<h1>iaatb.net</h1>
 		since 2002<br /><br />
-		<a href="/blog2/" style="color: #fff;"><span style="width:120px; height:170px; background-color:#ea2001; vertical-align: bottom; text-align: right; padding: 4px; border: 2px solid #fff; display: inline-block;">blog</span></a>
-		<a href="/style/"  style="color: #fff;"><span style="width:120px; height:170px; background-color:#913; vertical-align: bottom; text-align: right; padding: 4px; border: 2px solid #fff; display: inline-block;">style</span></a>
-		<a href="http://iaatb.net/pics/" style="color: #fff;"><span style="width:110px; height:170px; background-color:#99ccff; vertical-align: bottom; text-align: right; padding: 4px; border: 2px solid #fff; display: inline-block;">pics</span></a>
-		<a href="http://twitter.com/bennyfactor/"  style="color: #fff;"><span style="width:120px; height:170px; background-color:#6699cc; vertical-align: bottom; text-align: right; padding: 4px; border: 2px solid #fff; display: inline-block;">twitter</span></a>
-		<a href="http://github.com/bennyfactor"  style="color: #fff;"><span style="width:120px; height:170px; background-color:#336699; vertical-align: bottom; text-align: right; padding: 4px; border: 2px solid #fff; display: inline-block;">GitHub</span></a>
-	<a href="http://iaat.be/" style="color: #fff;"><span style="width:110px; height:170px; background-color:#003366; vertical-align: bottom; text-align: right; padding: 4px; border: 2px solid #fff; display: inline-block;">IAATBExpress</span></a>
+		<a href="/blog2/" class="blog"><span>blog</span></a>
+		<a href="/style/" class="style"><span>style</span></a>
+		<a href="/pics/" class="pics"><span>pics</span></a>
+		<a href="https://twitter.com/bennyfactor/" class="twitter"><span>twitter</span></a>
+		<a href="https://github.com/bennyfactor" class="github"><span>GitHub</span></a>
+		<a href="http://iaat.be/" class="express"><span>IAATBExpress</span></a>
 <br clear=all />
 </div>
 	</body>
